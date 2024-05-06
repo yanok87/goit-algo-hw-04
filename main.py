07a138352c955242092ca011658f652ce1ab35c0
Yana <yanok87@users.noreply.github.com>
2024-05-06 17:55:17 +0300

    Add analysis

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+"""This module compares three different sorting algorythms"""
+
 import timeit
+import random
 
 
-numbers = [5, 3, 8, 4, 2]
+# Creation of three different arrays of numbers
+unsorted_array = [random.randint(1, 100) for _ in range(100)]
+sorted_array = sorted(unsorted_array)
+A = sorted_array[: len(sorted_array) // 2]
+B = sorted_array[len(sorted_array) // 2 :]
+partially_sorted_array = B + A
 
 
 def insertion_sort(lst):
+    """This function implements the Insertion sort"""
     for i in range(1, len(lst)):
         key = lst[i]
         j = i - 1
@@ -15,11 +24,21 @@ def insertion_sort(lst):
     return lst
 
 
-execution_time_insertion = timeit.timeit(lambda: insertion_sort(numbers), number=1000)
-print("insertion_sort", execution_time_insertion)
+insertion_unsorted = timeit.timeit(lambda: insertion_sort(unsorted_array), number=1000)
+print("insertion_unsorted:", insertion_unsorted)
+
+insertion_partially_sorted = timeit.timeit(
+    lambda: insertion_sort(partially_sorted_array), number=1000
+)
+print("insertion_partially_sorted:", insertion_partially_sorted)
+
+insertion_sorted = timeit.timeit(lambda: insertion_sort(sorted_array), number=1000)
+print("insertion_sorted:", insertion_sorted)
 
 
 def merge_sort(arr):
+    """This function implements the Merge sort"""
+
     if len(arr) <= 1:
         return arr
 
@@ -34,6 +53,8 @@ def merge_sort(arr):
 
 
 def merge(left, right):
+    """This function implements the auxiliary merge of arrays"""
+
     result = []
     left_index, right_index = 0, 0
 
@@ -51,14 +72,36 @@ def merge(left, right):
     return result
 
 
-execution_time_merge = timeit.timeit(lambda: merge_sort(numbers), number=1000)
-print("merge_sort", execution_time_merge)
+merge_unsorted = timeit.timeit(lambda: merge_sort(unsorted_array), number=1000)
+print("merge_unsorted:", merge_unsorted)
+
+merge_partially_sorted = timeit.timeit(
+    lambda: merge_sort(partially_sorted_array), number=1000
+)
+print("merge_partially_sorted:", merge_partially_sorted)
+
+merge_sorted = timeit.timeit(lambda: merge_sort(sorted_array), number=1000)
+print("merge_sorted:", merge_sorted)
 
 
 def timsort(arr):
+    """This function uses the inbuilt Timsort algorythm"""
+
     sorted_arr = arr.sort()
     return sorted_arr
 
 
-execution_time_timsort = timeit.timeit(lambda: timsort(numbers), number=1000)
-print("timsort", execution_time_timsort)
+timsort_unsorted = timeit.timeit(lambda: timsort(unsorted_array), number=1000)
+print("timsort_unsorted:", timsort_unsorted)
+
+timsort_partially_sorted = timeit.timeit(
+    lambda: timsort(partially_sorted_array), number=1000
+)
+print("timsort_partially_sorted:", timsort_partially_sorted)
+
+timsort_sorted = timeit.timeit(lambda: timsort(sorted_array), number=1000)
+print("timsort_sorted:", timsort_sorted)
+
+"""ANALYSIS: ⁠For unsorted arrays: Timsort outperforms both Incertion and Merge sort.````
+⁠For partially sorted arrays: Timsort still has the lowest execution time, followed closely by Incertion and then Merge sort.
+⁠For sorted arrays: Timsort remains the fastest, with Insertion slightly slower, and Merge sort being the slowest among the three."""
